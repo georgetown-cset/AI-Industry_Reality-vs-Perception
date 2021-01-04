ecf7b97453d9b50c5aeb42a8248aa9c06972dc61
ep-cset <71038852+ep-cset@users.noreply.github.com>
2021-01-04 11:24:00 -0500

    Clarified C1 and C2 vs. Tech 1 and Tech 2

--- getTopEmployers.sql
+++ getTopEmployers.sql
@@ -1,10 +1,10 @@
 /* 
 
-generate table for a given NAICS2 or AI occupation classification 
+Generate table for a given NAICS2 or AI occupation classification 
 
-options for NAICS 2: Information, Manufacturing, Transportation and Warehousing, Professional, Scientific, and Technical Services, Real Estate and Rental and Leasing
+Options for NAICS 2: Information, Manufacturing, Transportation and Warehousing, Professional, Scientific, and Technical Services, Real Estate and Rental and Leasing
 
-options for AI occupation classification: C1, C2, PT, CT
+Options for AI occupation classification: C1 (Tech 1 in the paper), C2 (Tech 2 in the paper), PT, CT
 
 Below is an example query using Transportation and Warehousing with all AI occupations (`gcp-cset-projects.burning_glass.US_AI_framework_wDETAIL`). 
 Change the table for different occupation classifications and change the naics2_name for different naics 2 options. 
